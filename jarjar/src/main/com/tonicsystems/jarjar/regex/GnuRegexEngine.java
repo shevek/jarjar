c42308971b9dd7cd71d15a9f9784cdcd692589c3
herbyderby <herbyderby@2e6f0158-8533-0410-8474-b19e06cb3126>
2004-06-24 05:28:11 +0000

    Add license notices
    git-svn-id: http://jarjar.googlecode.com/svn/trunk@8 2e6f0158-8533-0410-8474-b19e06cb3126

--- jarjar/src/main/com/tonicsystems/jarjar/regex/GnuRegexEngine.java
+++ jarjar/src/main/com/tonicsystems/jarjar/regex/GnuRegexEngine.java
@@ -1,3 +1,23 @@
+/*
+  Jar Jar Links - A utility to repackage and embed third-party jar files
+  Copyright (C) 2004  Tonic Systems, Inc.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; see the file COPYING.  if not, write to
+  the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA 02111-1307 USA
+*/
+
 package com.tonicsystems.jarjar.regex;
 
 import gnu.regexp.RE;
