a99f34d96b405ac0d2d69bbe01822b9036111553
herbyderby <herbyderby@2e6f0158-8533-0410-8474-b19e06cb3126>
2004-08-28 22:11:57 +0000

    commit dependency killer classes
    git-svn-id: http://jarjar.googlecode.com/svn/trunk@9 2e6f0158-8533-0410-8474-b19e06cb3126

---
+++ jarjar/src/main/com/tonicsystems/jarjar/DepKill.java
@@ -0,0 +1,95 @@
+/*
+  Jar Jar Links - A utility to repackage and embed Java libraries
+  Copyright (C) 2004  Tonic Systems, Inc.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; see the file COPYING.  if not, write to
+  the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA 02111-1307 USA
+*/
+
+package com.tonicsystems.jarjar;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.jar.*;
+import net.sf.cglib.core.DebuggingClassWriter;
+import net.sf.cglib.transform.ClassReaderGenerator;
+import net.sf.cglib.transform.TransformingClassGenerator;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+
+public class DepKill
+{
+    public static void main(String[] args)
+    throws Exception
+    {
+        // long t = System.currentTimeMillis();
+        new DepKill(args);
+        // System.err.println("took " + (System.currentTimeMillis() - t) + " ms");
+    }
+
+    private DepKill(String[] args)
+    throws Exception
+    {
+        if (args.length < 2) {
+            System.err.println("Syntax: java com.tonicsystems.jarjar.DepKill <input-jar> <package-name> [<package-name> ...] > <output-jar>");
+            System.exit(1);
+        }
+            
+        String[] packageNames = new String[args.length - 1];
+        System.arraycopy(args, 1, packageNames, 0, packageNames.length);
+
+        
+        DepKillTransformer t = new DepKillTransformer(packageNames);
+        JarFile jar = new JarFile(args[0]);
+        JarOutputStream out = new JarOutputStream(new BufferedOutputStream(System.out));
+
+        byte[] buf = new byte[0x2000];
+        
+        for (Enumeration e = jar.entries(); e.hasMoreElements();) {
+            JarEntry entry = (JarEntry)e.nextElement();
+            InputStream in = jar.getInputStream(entry);
+
+            entry.setCompressedSize(-1);
+            out.putNextEntry(entry);
+            
+            if (entry.getName().endsWith(".class")) {
+                // System.err.println("processing " + entry.getName());
+
+                ClassReader reader = new ClassReader(in);
+                in.close();
+                
+                int[] version = reader.getVersion();
+                ClassWriter w = new DebuggingClassWriter(true, version[0], version[1]);
+                new TransformingClassGenerator(new ClassReaderGenerator(reader, null, false), t).generateClass(w);
+                out.write(w.toByteArray());
+            
+            } else {
+                pipe(in, out, buf);
+            }
+        }
+        out.close();
+    }
+
+    private static void pipe(InputStream in, OutputStream out, byte[] buf)
+    throws IOException
+    {
+        for (;;) {
+            int amt = in.read(buf);
+            if (amt < 0)
+                break;
+            out.write(buf, 0, amt);
+        }
+    }
+}
---
+++ jarjar/src/main/com/tonicsystems/jarjar/DepKillTransformer.java
@@ -0,0 +1,235 @@
+/*
+  Jar Jar Links - A utility to repackage and embed Java libraries
+  Copyright (C) 2004  Tonic Systems, Inc.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; see the file COPYING.  if not, write to
+  the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA 02111-1307 USA
+*/
+
+package com.tonicsystems.jarjar;
+
+import net.sf.cglib.core.Constants;
+import net.sf.cglib.core.Signature;
+import net.sf.cglib.transform.AbstractClassTransformer;
+import org.objectweb.asm.Attribute;
+import org.objectweb.asm.CodeAdapter;
+import org.objectweb.asm.CodeVisitor;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.Type;
+import java.util.*;
+
+class DepKillTransformer
+extends AbstractClassTransformer
+{
+    private String[] packageNames;
+
+    public DepKillTransformer(String[] packageNames)
+    {
+        this.packageNames = new String[packageNames.length];
+        for (int i = 0; i < packageNames.length; i++) {
+            // TODO: check that package is valid
+            StringBuffer sb = new StringBuffer();
+            sb.append('L');
+            sb.append(packageNames[i].replace('.', '/'));
+            if (sb.charAt(sb.length() - 1) != '/')
+                sb.append('/');
+            this.packageNames[i] = sb.toString();
+        }
+    }
+
+    private boolean checkDesc(String desc)
+    {
+        for (int i = 0; i < packageNames.length; i++) {
+            if (desc.startsWith(packageNames[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkMethodDesc(String methodDesc)
+    {
+        for (int i = 0; i < packageNames.length; i++) {
+            if (methodDesc.indexOf(packageNames[i]) >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String fixMethodDesc(String methodDesc)
+    {
+        Signature sig = new Signature("foo", methodDesc);
+        Type[] args = sig.getArgumentTypes();
+        for (int i = 0; i < args.length; i++)
+            args[i] = eraseType(args[i]);
+        return new Signature("foo", eraseType(sig.getReturnType()), args).getDescriptor();
+    }
+
+    private Type eraseType(Type type)
+    {
+        return checkDesc(type.getDescriptor()) ? Constants.TYPE_OBJECT : type;
+    }
+
+    private boolean checkName(String name)
+    {
+        return checkDesc("L" + name + ";");
+    }
+
+    private static void replace(CodeVisitor cv, String desc)
+    {
+        switch (desc.charAt(0)) {
+        case 'V':
+            break;
+        case 'D':
+            cv.visitInsn(Constants.DCONST_0);
+            break;
+        case 'F':
+            cv.visitInsn(Constants.FCONST_0);
+            break;
+        case 'J':
+            cv.visitInsn(Constants.LCONST_0);
+            break;
+        case 'C':
+        case 'S':
+        case 'B':
+        case 'I':
+        case 'Z':
+            cv.visitInsn(Constants.ICONST_0);
+            break;
+        case 'L':
+        case '[':
+            cv.visitInsn(Constants.ACONST_NULL);
+            break;
+        }
+    }
+
+    public CodeVisitor visitMethod(int access, String name, String desc, String[] exceptions, Attribute attrs)
+    {
+        // TODO: attrs?
+        if (exceptions != null) {
+            List exceptionList = new ArrayList(exceptions.length);
+            for (int i = 0; i < exceptions.length; i++) {
+                if (!checkName(exceptions[i]))
+                    exceptionList.add(exceptions[i]);
+            }
+            exceptions = (String[])exceptionList.toArray(new String[exceptionList.size()]);
+        }
+        return new DepKillCodeVisitor(cv.visitMethod(access, name, fixMethodDesc(desc), exceptions, attrs));
+    }
+
+    private class DepKillCodeVisitor
+    extends CodeAdapter
+    {
+        public DepKillCodeVisitor(CodeVisitor cv)
+        {
+            super(cv);
+        }
+
+        public void visitTypeInsn(int opcode, String desc)
+        {
+            if ((desc.charAt(0) == '[') ? checkDesc(desc) : checkName(desc)) {
+                // System.err.println("visitTypeInsn " + desc);
+                switch (opcode) {
+                case Constants.NEW:
+                case Constants.ANEWARRAY:
+                    cv.visitInsn(Constants.ACONST_NULL);
+                    break;
+                case Constants.CHECKCAST:
+                case Constants.INSTANCEOF:
+                    cv.visitInsn(Constants.ICONST_0);
+                    break;
+                }
+            } else {
+                cv.visitTypeInsn(opcode, desc);
+            }
+        }
+                
+        public void visitFieldInsn(int opcode, String owner, String name, String desc)
+        {
+            if (checkName(owner) || checkDesc(desc)) {
+                // System.err.println("visitFieldInsn " + owner + ", " + desc);
+                if (opcode == Constants.GETFIELD || opcode == Constants.PUTFIELD)
+                    cv.visitInsn(Constants.POP);
+                replace(cv, desc);
+            } else {
+                cv.visitFieldInsn(opcode, owner, name, desc);
+            }
+        }
+
+        public void visitMethodInsn(int opcode, String owner, String name, String desc)
+        {
+            if (checkName(owner)) {
+                // System.err.println("visitMethodInsn " + owner + ", " + desc + " (" + name + ")");
+                switch (opcode) {
+                case Constants.INVOKEINTERFACE:
+                case Constants.INVOKEVIRTUAL:
+                    cv.visitInsn(Constants.POP);
+                    break;
+                case Constants.INVOKESPECIAL:
+                    throw new DepKillException("Cannot remove invocation of " + owner + "." + desc);
+                case Constants.INVOKESTATIC:
+                }
+
+                Signature sig = new Signature(name, desc);
+                Type[] args = sig.getArgumentTypes();
+                for (int i = 0; i < args.length; i++)
+                    cv.visitInsn((args[i].getSize() == 2) ? Constants.POP2 : Constants.POP);
+                replace(cv, sig.getReturnType().getDescriptor());
+
+            } else if (checkMethodDesc(desc)) {
+                // System.err.println("visitMethodInsn " + owner + ", " + desc + " (" + name + ")");
+                desc = fixMethodDesc(desc);
+
+            } else {
+                cv.visitMethodInsn(opcode, owner, name, desc);
+            }
+        }
+
+        public void visitMultiANewArrayInsn(String desc, int dims)
+        {
+            if (checkDesc(desc)) {
+                // System.err.println("visitMultiANewArrayInsn " + desc);
+                cv.visitInsn(Constants.ACONST_NULL);
+            } else {
+                cv.visitMultiANewArrayInsn(desc, dims);
+            }
+        }
+
+        public void visitTryCatchBlock(Label start, Label end, Label handler, String type)
+        {
+            if (checkName(type)) {
+                // System.err.println("visitTryCatchBlock " + type);
+            } else {
+                cv.visitTryCatchBlock(start, end, handler, type);
+            }
+        }
+
+        public void visitLocalVariable(String name, String desc, Label start, Label end, int index)
+        {
+            if (checkDesc(desc)) {
+                // System.err.println("visitLocalVariable " + desc);
+                desc = Constants.TYPE_OBJECT.getDescriptor();
+            }
+            cv.visitLocalVariable(name, desc, start, end, index);
+        }
+
+        public void visitAttribute(Attribute attr)
+        {
+            // TODO?
+            cv.visitAttribute(attr);
+        }
+    }
+}
