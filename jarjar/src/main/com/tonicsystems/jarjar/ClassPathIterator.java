138c739b712d3810fca25433434bca4075802942
herbyderby <herbyderby@2e6f0158-8533-0410-8474-b19e06cb3126>
2005-02-18 19:37:25 +0000

    change ClassPathIterator to use system line.separator
    git-svn-id: http://jarjar.googlecode.com/svn/trunk@46 2e6f0158-8533-0410-8474-b19e06cb3126

--- jarjar/src/main/com/tonicsystems/jarjar/ClassPathIterator.java
+++ jarjar/src/main/com/tonicsystems/jarjar/ClassPathIterator.java
@@ -47,7 +47,7 @@ class ClassPathIterator implements Iterator
     
     public ClassPathIterator(File parent, String classPath) {
         this.parent = parent;
-        StringTokenizer st = new StringTokenizer(classPath, ":");
+        StringTokenizer st = new StringTokenizer(classPath, System.getProperty("path.separator"));
         while (st.hasMoreTokens()) {
             parts.add(st.nextElement());
         }
