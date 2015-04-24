4a77671326a37a7eaf9977fba4169ba04d2a8ce3
Shevek <shevek@anarres.org>
2015-04-24 14:49:57 -0700

    Start work on new dependency method.

--- jarjar-gradle/src/main/java/org/anarres/gradle/plugin/jarjar/JarjarController.java
+++ jarjar-gradle/src/main/java/org/anarres/gradle/plugin/jarjar/JarjarController.java
@@ -10,8 +10,11 @@ import groovy.lang.GroovyObjectSupport;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.annotation.Nonnull;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.internal.ClosureBackedAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This object appears as 'jarjar' in the project extensions.
@@ -20,11 +23,24 @@ import org.gradle.api.internal.ClosureBackedAction;
  */
 public class JarjarController extends GroovyObjectSupport {
 
+    private static final Logger LOG = LoggerFactory.getLogger(JarjarController.class);
     private static final AtomicInteger SEQ = new AtomicInteger(0);
     private final Project project;
 
     public JarjarController(@Nonnull Project project) {
         this.project = project;
+    }
+
+    @Nonnull
+    public Dependency dependency(Object dependencyNotation, Closure configurationClosure) {
+        Dependency d = project.getDependencies().create(dependencyNotation, configurationClosure);
+        LOG.info("sub is " + d);
+        return new JarjarDependency(d);
+    }
+
+    @Nonnull
+    public Dependency dependency(Object dependencyNotation) {
+        return dependency(dependencyNotation, Closure.IDENTITY);
     }
 
     @Nonnull
